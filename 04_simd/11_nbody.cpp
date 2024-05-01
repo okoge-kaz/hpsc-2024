d58d6fc1b91fffebe831949853ca33a50818f607
kazuki <kaz.tokyo.tech20@gmail.com>
2024-05-02 03:17:57 +0900

    feat: avx

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,26 +1,47 @@
+#include <immintrin.h>
+
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
 
 int main() {
   const int N = 8;
   float x[N], y[N], m[N], fx[N], fy[N];
-  for(int i=0; i<N; i++) {
+
+  for (int i = 0; i < N; i++) {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
-  for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
+
+  for (int i = 0; i < N; i++) {
+    __m256 x_i = _mm256_set1_ps(x[i]);
+    __m256 y_i = _mm256_set1_ps(y[i]);
+
+    __m256 x_j = _mm256_load_ps(x);
+    __m256 y_j = _mm256_load_ps(y);
+    __m256 m_j = _mm256_load_ps(m);
+
+    __m256 rx = _mm256_sub_ps(x_i, x_j);
+    __m256 ry = _mm256_sub_ps(y_i, y_j);
+    __m256 r_sq = _mm256_add_ps(_mm256_mul_ps(rx, rx), _mm256_mul_ps(ry, ry));
+    __m256 r = _mm256_sqrt_ps(r_sq);
+    __m256 r_cube = _mm256_mul_ps(r_sq, r);
+
+    __m256 mask = _mm256_cmp_ps(x_i, x_j, _CMP_NEQ_OQ);
+
+    __m256 force_x = _mm256_div_ps(_mm256_mul_ps(rx, m_j), r_cube);
+    __m256 force_y = _mm256_div_ps(_mm256_mul_ps(ry, m_j), r_cube);
+
+    force_x = _mm256_and_ps(force_x, mask);
+    force_y = _mm256_and_ps(force_y, mask);
+
+    // accumulate forces
+    for (int j = 0; j < 8; j++) {
+      fx[i] = fx[i] - force_x[j];
+      fy[i] = fy[i] - force_y[j];
     }
-    printf("%d %g %g\n",i,fx[i],fy[i]);
+    printf("%d %g %g\n", i, fx[i], fy[i]);
   }
 }
